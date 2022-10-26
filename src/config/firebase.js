7a0546a1d5814129564378b921b3ae7f042ef4a7
muftialfarisy <alfabjp@gmail.com>
2022-10-26 17:02:08 +0700

    update_api

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,8 +3,8 @@ import { getAuth } from "firebase/auth";
 
 // TODO: replace with your own config
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  // apiKey: "AIzaSyCr13XIM8kz-GKFEo3U-X4k1SEs0Ut5bFs",
+  // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  apiKey: "AIzaSyCr13XIM8kz-GKFEo3U-X4k1SEs0Ut5bFs",
   authDomain: "mydrakorlist.firebaseapp.com",
   projectId: "mydrakorlist",
   storageBucket: "mydrakorlist.appspot.com",
